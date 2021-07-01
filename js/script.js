24b9ea6ab275d3ba00a309aa38b892dde2ab78cd
maldrek3103 <cpisanu96@gmail.com>
2021-07-01 15:38:40 +0200

    started project, added variables

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// Variables
+var travelDistance = parseInt(prompt('Please insert the number of km you have to travel: '));
+var passengerAge = parseInt(prompt('Please insert you age: '));
+
+console.log(travelDistance, passengerAge);
+
+var ticketPriceEachKm = 0.21;
+
+console.log(ticketPriceEachKm);
