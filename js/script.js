e729700db32ad127aeeca14acf233520a9c3418c
maldrek3103 <cpisanu96@gmail.com>
2021-07-01 15:55:59 +0200

    created discount and final prices

--- js/script.js
+++ js/script.js
@@ -1,9 +1,24 @@
 // Variables
 var travelDistance = parseInt(prompt('Please insert the number of km you have to travel: '));
 var passengerAge = parseInt(prompt('Please insert you age: '));
-
-console.log(travelDistance, passengerAge);
-
 var ticketPriceEachKm = 0.21;
+var ticketCost = travelDistance * ticketPriceEachKm;
+var youngDiscount = ticketCost * (20 / 100);
+var oldDiscount = ticketCost * (40 / 100);
+var finalPrice;
 
+if (passengerAge < 18) {
+    finalPrice = ticketCost - youngDiscount;
+} else if (passengerAge >= 65) {
+    finalPrice = ticketCost - oldDiscount;
+} else {
+    finalPrice = ticketCost;
+}
+
+console.log(travelDistance);
+console.log(passengerAge);
 console.log(ticketPriceEachKm);
+console.log(ticketCost);
+// console.log(youngDiscount);
+// console.log(oldDiscount);
+console.log(finalPrice);
